bdcabaf2f6498f946497de6c640ff820dcb4e047
cyberpapi <robdezendorf@gmail.com>
2026-05-17 03:20:20 -0400

    plug 0.3.0 release asset URLs

--- plug.rb
+++ plug.rb
@@ -6,21 +6,21 @@ class Plug < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/cyberpapiii/plug/releases/download/v0.3.0/plug-mcp-aarch64-apple-darwin.tar.gz"
-      sha256 "31939d8c80ed132466aec1e6a30837bcecb0fdd3f59f0e1abef9774898d21f99"
+      url "https://github.com/cyberpapiii/plug/releases/download/v0.3.0/plug-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "85538afa1d0d50cf18e1341a3abba44d00a6548e0b5458c23b631f5af07ef2e5"
     else
-      url "https://github.com/cyberpapiii/plug/releases/download/v0.3.0/plug-mcp-x86_64-apple-darwin.tar.gz"
-      sha256 "d99126cad1f826c39e2b7a1610d8a318c70e1fd24185b8fb869b045c5b2b5a2b"
+      url "https://github.com/cyberpapiii/plug/releases/download/v0.3.0/plug-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "14c5f240f99d9b808ccd52aa18479a559342991f053acc47430426a35c181103"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/cyberpapiii/plug/releases/download/v0.3.0/plug-mcp-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "c80859483d8889d6ce96f95db4f17b4982d805875c2f0094a1d5125491651663"
+      url "https://github.com/cyberpapiii/plug/releases/download/v0.3.0/plug-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "ec416794fbc97ca16c476e2d8ce3e1fcd312ce1dfb4534857deefd5c8bab846e"
     else
-      url "https://github.com/cyberpapiii/plug/releases/download/v0.3.0/plug-mcp-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b0cc08e98315247b41fcace2b10bc64ebbeacaa4be6cfbfd0506c087d08f6e65"
+      url "https://github.com/cyberpapiii/plug/releases/download/v0.3.0/plug-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "e73bd9fc4a3e8fa0db4a343bbb8465d2ecbecc5012435bd58681935f4523791b"
     end
   end
 
